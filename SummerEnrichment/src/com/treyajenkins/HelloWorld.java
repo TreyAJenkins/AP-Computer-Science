693fc19e45bc259d94494aba0bc8534723cdfe28
TreyAJenkins <trey@treycorp.com>
2019-06-01 22:46:42 -0400

    Finished Hello World

---
+++ SummerEnrichment/src/com/treyajenkins/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.treyajenkins;
+
+public class HelloWorld {
+    public static String identity = "Lesson: Hello World | Project: From Me to You";
+
+    public static void main() {
+        System.out.println("From: Bill Smith\nAddress: Dell Computer, Bldg 13\nDate: April 12, 2005\n\nTo: Jack Jones\n\nMessage: Help! I'm trapped inside a computer!");
+    }
+}
