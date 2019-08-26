df01ff066618bd102a80df5fc2821d5a357194df
TreyAJenkins <trey@treycorp.com>
2019-08-26 13:24:43 -0400

    Finished BankBalance, Chapter 1, Program 1

--- BankBalance/src/com/treyajenkins/Main.java
+++ BankBalance/src/com/treyajenkins/Main.java
@@ -1,15 +1,18 @@
 package com.treyajenkins;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
-    double initialBalance = 1000.00;
-    double interestRate = 0.05;
+    static double initialBalance = 1000.00;
+    static double interestRate = 0.05;
     static int years = 3;
 
     public static void main(String[] args) {
-        for (int i = 1; i <= years; i++) {
-
-            System.out.println("Balance after year " + String.valueOf(i) + " = $");
+        for (int year = 1; year <= years; year++) {
+            DecimalFormat df = new DecimalFormat("0.00"); // Format balance to two decimal places
+            // Prints and calculates interest
+            System.out.println("Balance after year " + year + " = $" + df.format(initialBalance * Math.pow(1+interestRate, year)));
         }
     }
 }
