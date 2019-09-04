3593b82d54bf7ec958105a735687af7c75e22589
TreyAJenkins <trey@treycorp.com>
2019-09-04 10:16:01 -0400

    Created AverageInt

---
+++ AverageInt/src/AverageInt.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Trey Jenkins on September 04, 2019
+ *
+ * Objective:
+ *      Write an application that prompts the user to enter in three int values. Create and read
+ *      three int variables from the user. Calculate their average and store it in an int variable. Print out the
+ *      average as an int value.
+ */
+public class AverageInt {
+}
