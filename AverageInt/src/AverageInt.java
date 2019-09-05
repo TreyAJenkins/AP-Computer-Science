7ecc6c4e3d8db731548523281735b0ec9a68ae9e
TreyAJenkins <trey@treycorp.com>
2019-09-05 09:59:16 -0400

    Finished AverageInt

--- AverageInt/src/AverageInt.java
+++ AverageInt/src/AverageInt.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Trey Jenkins on September 04, 2019
  *
@@ -7,4 +9,22 @@
  *      average as an int value.
  */
 public class AverageInt {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Initialize the scanner
+
+        System.out.print("Enter 1st number: ");
+        int uno = scanner.nextInt(); // Read in the first int
+
+        System.out.print("Enter 2nd number: ");
+        int dos = scanner.nextInt(); // Read in the second int
+
+        System.out.print("Enter 3rd final number: ");
+        int tres = scanner.nextInt(); // Read in the last int
+
+        int average = (uno + dos + tres) / 3; // Calculate the average
+
+        System.out.println("The incorrect average is " + average);
+    }
+
 }
