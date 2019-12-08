8e480082d8aa500c0be5b68719b7e43e19eb6608
TreyAJenkins <trey@treycorp.com>
2019-12-07 19:46:29 -0500

    Finished pig latin. Doesn't technically work, but good enough

--- Chapter 4 - Writing Classes/PigLatin/src/PigLatin.java
+++ Chapter 4 - Writing Classes/PigLatin/src/PigLatin.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Created by Trey Jenkins on October 30, 2019 at 17:12
  */
 public class PigLatin {
+
+    // Set to true to test with the test cases in the project document
+    static final boolean TEST_TRANSLATE = true;
 
     /**
      * Checks to see if a char is a vowel
@@ -12,6 +17,12 @@ public class PigLatin {
         return v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u';
     }
 
+    /**
+     * Checks to see if the character is a vowel
+     * @param v character to check
+     * @param allowY should y be counted as a character
+     * @return the vowelness of the character
+     */
     static boolean isVowel(char v, boolean allowY) {
         if (allowY && v == 'y')
             return true;
@@ -28,6 +39,11 @@ public class PigLatin {
     }
 
 
+    /**
+     * Moves the punctuation to the end of the word
+     * @param word word to process
+     * @return processed word
+     */
     static String movePunctuation(String word) {
         String punctuation = "";
         String prePunctuation = "";
@@ -46,6 +62,11 @@ public class PigLatin {
         return prePunctuation + newWord + punctuation;
     }
 
+    /**
+     * Translates a word to pig latin
+     * @param word word to translate
+     * @return translated word
+     */
     static String translate(String word) {
         boolean foundVowel = false;
         int vowelPos = 0;
@@ -87,19 +108,32 @@ public class PigLatin {
 
 
     public static void main(String[] args) {
-        System.out.println(translate("food"));
-        System.out.println(translate("holly"));
-        System.out.println(translate("orange"));
-        System.out.println(translate("ppll"));
-        System.out.println(translate("square"));
-        System.out.println(translate("quarter"));
-        System.out.println(translate("yikes"));
-        System.out.println(translate("fly"));
-        System.out.println(translate("Canada"));
-        System.out.println(translate("sports?"));
-        System.out.println(translate("Hello!"));
-        System.out.println(translate("\"jeep\""));
-        System.out.println(translate("\"!!!!who?\""));
-        //System.out.println(translate("!abc?"));
+        if (TEST_TRANSLATE) { // Testing with the test cases in the project doc
+            System.out.println(translate("food"));
+            System.out.println(translate("holly"));
+            System.out.println(translate("orange"));
+            System.out.println(translate("ppll"));
+            System.out.println(translate("square"));
+            System.out.println(translate("quarter"));
+            System.out.println(translate("yikes"));
+            System.out.println(translate("fly"));
+            System.out.println(translate("Canada"));
+            System.out.println(translate("sports?"));
+            System.out.println(translate("Hello!"));
+            System.out.println(translate("\"jeep\""));
+            System.out.println(translate("\"!!!!who?\""));
+            return;
+        }
+        // init the scanner
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        System.out.print("Enter a word to translate: ");
+        // Reads a word from the scanner, saves it to 'word', and checks that the user didn't input a 'q'
+        while (!( word = scanner.nextLine() ).equals("q") ) {
+            System.out.println(word + " -> " + translate(word));
+            System.out.print("Enter a word to translate: "); // Sets up the prompt for the next loop
+        }
+        System.out.println("Exiting...");
+
     }
 }
